510311ca3dd2b1bdbbab95c92c1d48350e531c43
KarthikeyanEzhumalai <karthikjks513@gmail.com>
2023-08-16 21:53:47 +0530

    Sample Git Project2 Coding Done

---
+++ src/org/com/SampleGitProject.java
@@ -0,0 +1,18 @@
+package org.com;
+
+public class SampleGitProject {
+	public static void main(String[] args) {
+		
+		String s= "jA23Va";
+		String r="";
+		for (int i = 0; i < s.length(); i++) {
+			
+			char ch = s.charAt(i);
+			
+		}	
+	
+		
+	}	
+	
+
+}
